9907edb82373698a2a487a2b8fde88bbc3a4478e
Jean-Philippe Menil <jpmenil@gmail.com>
2024-08-26 15:16:34 +0200

    update
    Signed-off-by: Jean-Philippe Menil <jpmenil@gmail.com>

--- keyboards/bastardkb/charybdis/3x5/keymaps/jpmenil/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/jpmenil/keymap.c
@@ -61,9 +61,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         A_MT   , S_MT   , D_MT   , F_MT   , KC_G   ,                           KC_H   , J_MT   , K_MT   , L_MT   , SCLN_MT,
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
         KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                           KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,
-    //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
-                                        MO(5)  , LT_3, KC_SPC,         KC_ENT ,     LT_2
-    //                                └────────┘   └────────┘       └────────┘   └────────┘
+    //└────────┴────────┴────┬───┴────┬───┼────────┼───┬────────┐   ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                               MO(5)  ,       LT_3 ,      KC_SPC,     KC_ENT ,     LT_2
+    //                       └────────┘   └────────┘   └────────┘   └────────┘   └────────┘
     ),
 
     [1] = LAYOUT_charybdis_3x5(
@@ -73,9 +73,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         A_MT   , R_MT   , S2_MT  , T_MT   , KC_G   ,                           KC_M   , N_MT   , E_MT   , I_MT   , O_MT   ,
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
         KC_Z   , KC_X   , KC_C  ,  KC_D   , KC_V   ,                           KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH,
-    //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
-                                        _______,LT_3,KC_SPC ,         KC_ENT ,     LT_2
-    //                                └────────┘   └────────┘       └────────┘   └────────┘
+    //└────────┴────────┴────┬───┴────┬───┼────────┼───┬────────┐   ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                               _______,      LT_3  ,     KC_SPC ,     KC_ENT ,     LT_2
+    //                       └────────┘   └────────┘   └────────┘   └────────┘   └────────┘
     ),
 
     [2] = LAYOUT_charybdis_3x5(
@@ -85,21 +85,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         A_MT   , S_MT   , D_MT   , F_MT   ,_______ ,                           KC_MINS, KC_EQL , KC_LCBR, KC_RCBR, KC_PIPE,
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
        _______ ,_______ ,_______ ,_______ ,_______ ,                           KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_GRV ,
-    //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
-                                        _______,MO(4),QK_GESC,        _______ ,    _______
-    //                                └────────┘   └────────┘       └────────┘   └────────┘
+    //└────────┴────────┴────┬───┴────┬───┼────────┼───┬────────┐   ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                               _______,      MO(4) ,     QK_GESC,    _______ ,    _______
+    //                       └────────┘   └────────┘   └────────┘   └────────┘   └────────┘
     ),
 
     [3] = LAYOUT_charybdis_3x5(
     //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
         KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                           KC_6   , KC_7   , KC_8   , KC_9   , KC_0   ,
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
-        WM1    , WM2    , WM(3)  , WM(4)  , WM(5)  ,                           KC_LEFT, KC_DOWN, KC_UP  ,KC_RIGHT, KC_QUOT,
+        WM1    ,   WM2  , WM(3)  , WM(4)  , WM(5)  ,                           KC_LEFT, KC_DOWN, KC_UP  ,KC_RIGHT, KC_QUOT,
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
         WM(6)  , WM(7)  , WM(8)  , WM(9)  , WM(0)  ,                           KC_HOME, KC_PGUP, KC_PGDN, KC_DOT , KC_BSLS,
-    //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
-                                       _______ ,    _______ , _______,        _______ ,     MO(4)
-    //                                └────────┘   └────────┘       └────────┘   └────────┘
+    //└────────┴────────┴────┬───┴────┬───┼────────┼───┬────────┐   ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                              _______ ,    _______ ,     _______,    _______ ,     MO(4)
+    //                       └────────┘   └────────┘   └────────┘   └────────┘   └────────┘
     ),
 
     [4] = LAYOUT_charybdis_3x5(
@@ -109,9 +109,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        E_CLOSE , E_SAVE , E_UNDO , E_IDT  ,        ,                           WZ_LEFT, WZ_DOWN, WZ_UP  ,WZ_RIGHT, M_BLOCK,
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
         E_RS   ,_______ , E_BOB  , E_EOB  ,_______ ,                          WZ_SPLIH, WZ_SLCT, W_BACK , W_FWD  ,WZ_SPLIV,
-    //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
-                                       _______ ,    _______ , _______,        _______ ,    _______
-    //                                └────────┘   └────────┘       └────────┘   └────────┘
+    //└────────┴────────┴────┬───┴────┬───┼────────┼───┬────────┐   ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                              _______ ,    _______ ,     _______,    _______ ,    _______
+    //                       └────────┘   └────────┘   └────────┘   └────────┘   └────────┘
     ),
 
     [5] = LAYOUT_charybdis_3x5(
@@ -121,83 +121,71 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        _______ , DRGSCRL , KC_BTN1,KC_BTN2 ,_______ ,                          _______ ,_______ ,_______ ,_______ ,_______ ,
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
        _______ ,_______ ,_______ ,_______ ,_______ ,                          _______ ,_______ ,_______ ,_______ ,_______ ,
-    //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
-                                       _______ ,    _______ , _______,        _______ ,    _______
-    //                                └────────┘   └────────┘       └────────┘   └────────┘
+    //└────────┴────────┴────┬───┴────┬───┼────────┼───┬────────┐   ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                              _______ ,    _______ ,     _______,    _______ ,    _______
+    //                       └────────┘   └────────┘   └────────┘   └────────┘   └────────┘
     ),
 
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        switch (keycode) {
+            case E_BOB:
+                SEND_STRING(SS_TAP(X_ESC) "<");
+                return false;
+            case E_CLOSE:
+                SEND_STRING(SS_LCTL("x")"#");
+                return false;
+            case E_EOB:
+                SEND_STRING(SS_TAP(X_ESC) ">");
+                return false;
+            case E_IDT:
+                SEND_STRING(SS_LCTL("x")SS_TAP(X_TAB));
+                return false;
+            case E_RS:
+                SEND_STRING(SS_RALT("%"));
+                return false;
+            case E_SAVE:
+                SEND_STRING(SS_LCTL("xs"));
+                return false;
+            case E_UNDO:
+                SEND_STRING(SS_LCTL("x")"u");
+                return false;
+            case M_BLOCK:
+                SEND_STRING("```");
+                return false;
+            case W_BACK:
+                SEND_STRING(SS_LGUI(SS_TAP(X_LEFT)));
+                return false;
+            case W_FWD:
+                SEND_STRING(SS_LGUI(SS_TAP(X_RIGHT)));
+                return false;
+            case WZ_DOWN:
+                SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_DOWN))));
+                return false;
+            case WZ_LEFT:
+                SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_LEFT))));
+                return false;
+            case WZ_RIGHT:
+                SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_RIGHT))));
+                return false;
+            case WZ_SLCT:
+                SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_X))));
+                return false;
+            case WZ_SPLIH:
+                SEND_STRING(SS_LCTL(SS_LSFT(SS_LALT(SS_TAP(X_QUOTE)))));
+                return false;
+            case WZ_SPLIV:
+                SEND_STRING(SS_LCTL(SS_LSFT(SS_LALT(SS_TAP(X_5)))));
+                return false;
+            case WZ_UP:
+                SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_UP))));
+                return false;
+        }
+    }
+
     switch (keycode) {
-        case E_BOB:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_TAP(X_ESC) "<");
-            return false;
-        case E_CLOSE:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_LCTL("x")"#");
-            return false;
-        case E_EOB:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_TAP(X_ESC) ">");
-            return false;
-        case E_IDT:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_LCTL("x")SS_TAP(X_TAB));
-            return false;
-        case E_RS:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_RALT("%"));
-            return false;
-        case E_SAVE:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_LCTL("xs"));
-            return false;
-        case E_UNDO:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_LCTL("x")"u");
-            return false;
-        case M_BLOCK:
-            if (!record->event.pressed) return true;
-            SEND_STRING("```");
-            return false;
-        case W_BACK:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_LGUI(SS_TAP(X_LEFT)));
-            return false;
-        case W_FWD:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_LGUI(SS_TAP(X_RIGHT)));
-            return false;
-        case WZ_DOWN:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_DOWN))));
-            return false;
-        case WZ_LEFT:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_LEFT))));
-            return false;
-        case WZ_RIGHT:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_RIGHT))));
-            return false;
-        case WZ_SLCT:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_X))));
-            return false;
-        case WZ_SPLIH:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_LCTL(SS_LSFT(SS_LALT(SS_TAP(X_QUOTE)))));
-            return false;
-        case WZ_SPLIV:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_LCTL(SS_LSFT(SS_LALT(SS_TAP(X_5)))));
-            return false;
-        case WZ_UP:
-            if (!record->event.pressed) return true;
-            SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_UP))));
-            return false;
         case WM1:
             if (record->tap.count && record->event.pressed) {
                 tap_code16(LGUI(KC_1));
@@ -221,6 +209,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
     }
+
     return true;
 }
 
@@ -228,10 +217,9 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LCTL_T(KC_K):
         case LGUI_T(KC_J):
-            /* return TAPPING_TERM - 50; */
         case KC_MINS:
         case SCLN_MT:
-            /* return TAPPING_TERM - 50; */
+            return TAPPING_TERM - 50;
         default:
             return TAPPING_TERM;
     }
